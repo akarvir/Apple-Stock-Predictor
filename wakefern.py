d84eb9213a1625befbf7ce58ef6106830e7002d3
akarvir <141398134+akarvir@users.noreply.github.com>
2024-08-15 09:15:08 +0530

    First commit

--- wakefern.py
+++ wakefern.py
@@ -1,121 +1,177 @@
-import pandas as pd 
+
+ #Predicting local min and local max of Apple stock to buy or short stock accordingly. 
+
+
+import yfinance as yf
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt  
-import seaborn as sns  
- 
-import tensorflow as tf
-from tensorflow import keras
-from keras import Input
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, SimpleRNN, LSTM, GRU
- 
-import yfinance as yf  
-from sklearn.model_selection import train_test_split # 
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
-company = 'AAPL'  
-stock_data = yf.Ticker(company)
+# Simple strategy. If green point, we buy more stock. If not, we sell.
+apple_ticker = yf.Ticker("AAPL")
 
- 
-stock_history = stock_data.history(period='max',interval='1d')  
+stock_history = apple_ticker.history(period = 'max',interval = '1d')
 
-closing_prices = stock_history['Close']
+# for each day, check the day ahead of it. This is for the model training data.
 
+# The input parameters will be - volume, 3 day, 10 day, 20 day regression coefficient, normalized stock price. These are technical indicators
 
-
-training_data, testing_data = train_test_split(stock_history,test_size=0.2,random_state=42,shuffle=True) # Splitting the data. Decided to keep 20 percent testing
-# training data
-X_train = training_data.drop(['Close','Volume','Dividends','Stock Splits'],axis=1)
-y_train = training_data['Close']
-# testing data
- 
-X_test = testing_data.drop(['Close','Volume','Dividends','Stock Splits'],axis=1)
-y_test = testing_data['Close']
-
-ann_model = Sequential()
-ann_model.add(Dense(25,activation='relu',kernel_initializer='he_normal',bias_initializer='ones',input_shape=(X_train.shape[1],1)))
-ann_model.add(Dropout(rate=0.2))
-ann_model.add(Dense(50,activation='relu',kernel_initializer='he_normal',bias_initializer='ones'))
-ann_model.add(Dropout(rate=0.2))
-ann_model.add(Dense(25,activation='relu',kernel_initializer='he_normal',bias_initializer='ones'))
-ann_model.add(Dense(1,activation='relu',kernel_initializer='he_normal',bias_initializer='ones'))
-
-# ann_model.summary() 
-
-optimizer = keras.optimizers.SGD(learning_rate=0.03)
-loss_function = keras.losses.MeanSquaredError()
-ann_model.compile(optimizer=optimizer,loss=loss_function)
-
-history = ann_model.fit(X_train,y_train,batch_size=32,epochs=50,shuffle=True)
-
-testing_loss = ann_model.evaluate(X_test,y_test,batch_size=32,verbose=0)
-print(f'Testing Loss in Dollars: ${np.sqrt(testing_loss)}') # About 41 dollars.
-
-# RNN next
+# 1 implies is a buying point, 0 implies not.
 
 
-raw_sequence_data_X = []
-raw_sequence_data_Y = []
-data_list = stock_history.drop(['Volume','Dividends','Stock Splits'],axis=1).values
- # padding the image
-for i in range(len(data_list)):
-  sequence = [[0]*4] * 10 #  
-
- 
-  if i < 10:
-    for x in range(i):
-      sequence[x] = list(data_list[x])
-  else:
-    index = 0
-    for x in range(i-10,i):
-      sequence[index] = list(data_list[x])
-      index += 1
-  raw_sequence_data_X.append(sequence)
-  raw_sequence_data_Y.append(data_list[i][3])
-
-# Changing the lists into numpy arrays
-sequence_X = np.array(raw_sequence_data_X)
-sequence_Y = np.array(raw_sequence_data_Y)
-
-#splitting the sequence data now.
-
-X_train, X_test, y_train, y_test = train_test_split(sequence_X,sequence_Y,test_size=0.2,random_state=42,shuffle=True)
+# now, creating the new dataset.
 
 
-rnn_model = Sequential()
+columns = ['Volume','3dayreg','10dayreg','20dayreg','normalizedsp','is_buyingpoint'] # the technical indicators serving as the input parameters.
 
- 
-rnn_model.add(SimpleRNN(50,activation='tanh',bias_initializer='ones',return_sequences=False,input_shape=(X_train.shape[1],X_train.shape[2])))
+dataset = pd.DataFrame(columns = columns)
 
- 
-rnn_model.add(Dense(25,activation='relu',kernel_initializer='he_normal',bias_initializer='ones'))
-rnn_model.add(Dense(1,activation='relu',kernel_initializer='he_normal',bias_initializer='ones')) #Adding layers
+for i in range(20,len(stock_history)-1):
 
-optimizer = keras.optimizers.SGD(learning_rate=0.001)
-loss_function = keras.losses.MeanSquaredError()
-rnn_model.compile(optimizer=optimizer,loss=loss_function)
+  currentdayprice = stock_history.iloc[i]['Close']
 
-#training the RNN now
-history = rnn_model.fit(X_train,y_train,batch_size=32,epochs=5,shuffle=True)
+  nextdayprice = stock_history.iloc[i+1]['Close']
 
-testing_loss = rnn_model.evaluate(X_test,y_test,batch_size=32,verbose=0)
-print(f'Testing Loss in Dollars: ${np.sqrt(testing_loss)}') #About 30 dollars. way better than the ANN !
+  if(nextdayprice>currentdayprice): buying_point = 1
+  else: buying_point = 0
 
-# THE LSTM - Long Short-Term Network
+  threexvalues = []
 
-lstm_model = Sequential()
-lstm_model.add(LSTM(30,activation='tanh',recurrent_activation='sigmoid',bias_initializer='ones',return_sequences=False,input_shape=(X_train.shape[1],X_train.shape[2])))
+  for j in range(1,4):
+    threexvalues.append([j])
 
-# At the end of our LSTM, we will get a 50 dimensional vector as our output.
-# We need a single number so we will add an ANN on top of this RNN.
-lstm_model.add(Dense(25,activation='relu',kernel_initializer='he_normal',bias_initializer='ones'))
-lstm_model.add(Dense(1,activation='relu',kernel_initializer='he_normal',bias_initializer='ones'))
+  # getting y values of last 3 days.
+  threeyvalues = []
 
-#Writing the configuration for how the model learns
+  for m in range(i-3,i):
 
-optimizer = keras.optimizers.SGD(learning_rate=0.001)
-loss_function = keras.losses.MeanSquaredError()
-lstm_model.compile(optimizer=optimizer,loss=loss_function)
+    threeyvalues.append(stock_history.iloc[m]['Close'])
 
-testing_loss = lstm_model.evaluate(X_test,y_test,batch_size=32,verbose=0)
-print(f'Testing Loss in Dollars: ${np.sqrt(testing_loss)}') # About 29 dollars. Slightly better than the RNN
+
+  threedayregression = LinearRegression().fit(threexvalues,threeyvalues)
+
+  threedaycoef = threedayregression.coef_ # got the 3-day regression coefficient.
+
+  tenxvalues = []
+
+  tenyvalues = []
+
+  for f in range(1,11):
+    tenxvalues.append([f]) # got the x values for the 10-day Linear regression
+
+  for v in range(i-10,i):
+    tenyvalues.append(stock_history.iloc[v]['Close'])
+
+  tendayregression = LinearRegression().fit(tenxvalues,tenyvalues)
+
+  tendaycoef = tendayregression.coef_
+
+  twentyxvalues = []
+
+  twentyyvalues = []
+
+  for c in range(1,21): twentyxvalues.append([c])
+
+  for x in range(i-20,i):
+    twentyyvalues.append(stock_history.iloc[x]['Close'])
+
+  twentydayregression = LinearRegression().fit(twentyxvalues, twentyyvalues)
+
+  twentydaycoef = twentydayregression.coef_
+
+  day_volume = stock_history.iloc[i]['Volume']
+
+  if((stock_history.iloc[i]['High'] - stock_history.iloc[i]['Low'])==0): normalizedstockprice = 0
+  else: normalizedstockprice = (stock_history.iloc[i]['Close'] - stock_history.iloc[i]['Low'])/(stock_history.iloc[i]['High'] - stock_history.iloc[i]['Low'])
+
+
+
+  row = {'Volume' : day_volume, '3dayreg' : threedaycoef,'10dayreg':tendaycoef, '20dayreg':twentydaycoef,'normalizedsp': normalizedstockprice,'is_buyingpoint' : buying_point}
+
+
+  dataset.loc[len(dataset)] = row
+
+
+dataset.dropna(subset=['normalizedsp'], inplace=True)
+
+
+
+# Implementing the model and optimizing the threshold.
+
+
+from sklearn.metrics import accuracy_score
+
+y_values = dataset['is_buyingpoint']
+
+x_values = dataset.drop('is_buyingpoint',axis = 1)
+
+x_train,x_test,y_train,y_test = train_test_split(x_values,y_values,test_size = 0.2, random_state = 42, shuffle = True)
+
+
+
+
+model = LogisticRegression()
+
+model.fit(x_train,y_train) # training the model
+
+y_probs = model.predict_proba(x_test)[:, 1] # getting the respective probabilities. Threshold will determine the outcome sequence.
+
+print(len(y_probs))
+
+thresholds = np.arange(0.71,0.0, -0.01).tolist() # all thresholds to test.
+
+noofbuyingpoints = 0
+
+missbuyingpoints = 0
+
+
+
+
+for threshold in thresholds:
+
+  incorrect_counter = 0
+
+  y_pred = (y_probs>=threshold).astype(int) # a different outcome sequence for each threshold
+
+  misclassification_counter = 0
+
+  for i in range(len(y_pred)):
+
+
+    if(y_test.iloc[i]==1): noofbuyingpoints +=1
+    if(y_test.iloc[i]==1 and y_pred[i]==0): missbuyingpoints +=1
+    if(y_test.iloc[i]==0 and y_pred[i]==1): misclassification_counter +=1
+    if(y_test.iloc[i]!=y_pred[i]): incorrect_counter +=1
+
+
+  print("The missclassification counter for this threshold ",threshold," is ",misclassification_counter)
+  print("With this threshold we make the right prediction ",((incorrect_counter)/(len(y_pred)))*100,"% of the time")
+  print("Out of the times we make missclassification errors, we buy high and sell low ",((misclassification_counter)/(incorrect_counter))*100,"% of the errors! ")
+  print("The model misses buying points",(missbuyingpoints/noofbuyingpoints)*100,"% of the time")
+  for m in range(2): print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
